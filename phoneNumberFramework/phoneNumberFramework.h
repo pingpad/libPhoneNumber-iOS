2dc24a28a4d76bc77ccc1ec252d39a90c94cf47d
David HM Spector <spector@zeitgeist.com>
2015-02-05 17:52:44 -0800

    - First cut of gluing the files together for an iOS8+ dyanic framework.

---
+++ phoneNumberFramework/phoneNumberFramework.h
@@ -0,0 +1,27 @@
+//
+//  phoneNumberFramework.h
+//  phoneNumberFramework
+//
+//  Created by David HM Spector on 2/5/15.
+//  Copyright (c) 2015 NHN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for phoneNumberFramework.
+FOUNDATION_EXPORT double phoneNumberFrameworkVersionNumber;
+
+//! Project version string for phoneNumberFramework.
+FOUNDATION_EXPORT const unsigned char phoneNumberFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <phoneNumberFramework/PublicHeader.h>
+
+
+#import <phoneNumberFramework/NBAsYouTypeFormatter.h>
+#import <phoneNumberFramework/NBPhoneNumberUtil.h>
+#import <phoneNumberFramework/NBNumberFormat.h>
+#import <phoneNumberFramework/NBPhoneNumber.h>
+#import <phoneNumberFramework/NBPhoneNumberDesc.h>
+#import <phoneNumberFramework/NBPhoneNumberDefines.h>
+#import <phoneNumberFramework/NBPhoneMetaData.h>
+#import <phoneNumberFramework/NSArray+NBAdditions.h>
